dbe2a4e53f01c9991c0d30fc630fcb6a332616f9
moshkin <moshkin@finch-melrose.com>
2016-12-01 13:32:24 +0300

    refactoring

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,14 +1,15 @@
 import React, {PropTypes, Component} from 'react';
 import {connect} from 'react-redux';
-import { bindActionCreators } from 'redux'
+import {bindActionCreators} from 'redux'
 import * as actionCreators from '../actions/actionCreator';
 import ComponentJSON from '../components/ComponentJSON'
+import elements from '../elements'
 
 class NotFound extends Component {
 
   constructor(props) {
     super(props);
-    const { fetchModel } = this.props.actions;
+    const {fetchModel} = this.props.actions;
     this.fetchModel = fetchModel;
   }
 
@@ -19,26 +20,27 @@ class NotFound extends Component {
   componentWillReceiveProps(next) {
     const url = this.props.params.splat;
     const nextUrl = next.params.splat;
-    if(url !== nextUrl) {
+    if (url !== nextUrl) {
       this.fetchModel();
     }
   }
 
   render() {
-    const { response } = this.props;
+    const {response} = this.props;
     let Element = null;
     let data;
     let responseClass;
     if (typeof(response) === "object" && response !== null) {
       responseClass = response['@class'].split('.').pop();
-      try{
-        Element = require(`../components/${responseClass}/index`).default;
-        data = response.data;
-      }
-      catch(err) {
+
+      if (elements[responseClass] === undefined) {
         return (
           <ComponentJSON>{JSON.stringify(response, "", 4)}</ComponentJSON>
         );
+      }
+      else {
+        Element = elements[responseClass];
+        data = response.data;
       }
     }
     else {
@@ -53,7 +55,7 @@ class NotFound extends Component {
 }
 
 const mapStateToProps = state => {
-  const { response } = state.main;
+  const {response} = state.main;
   return {
     response
   }
